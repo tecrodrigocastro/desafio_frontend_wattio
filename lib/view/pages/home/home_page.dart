c3976078f552173d43fe01e538b5e5088e962bac
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2022-08-18 20:10:17 -0300

    Home Page

--- lib/view/pages/home/home_page.dart
+++ lib/view/pages/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -28,14 +30,56 @@ class _HomePageState extends State<HomePage> {
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
-                  children: const [
-                    Text(
+                  children: [
+                    const Text(
                       'Calcule a sua economia',
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 22,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
+                    const Text(
+                      'O valor da minha conta de luz mensal é:',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      color: Colors.white,
+                      height: 50,
+                      width: isMobile(context) ? sizeWidth : sizeWidth / 2,
+                      child: TextFormField(
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        initialValue: '1000',
+                        decoration: const InputDecoration(
+                          prefixText: 'R\$',
+                          prefixStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        elevation: 4,
+                        shadowColor: Colors.red,
+                        primary: Colors.white,
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Calcular economia',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    )
                   ],
                 ),
               ),
